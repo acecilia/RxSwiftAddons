07e37a930fafb63d53cb46bbaabb11bcbfd87fc6
acecilia <a.cecilia.luque@gmail.com>
2016-07-12 04:28:35 +0200

    Improved implementation of postBuild.sh

--- postBuild.sh
+++ postBuild.sh
@@ -3,22 +3,39 @@ TAGS="TODO:|FIXME:"
 find "${SRCROOT}/Sources" \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -print0 | xargs -0 egrep -s --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
 
 #Set podspec values based on xcode project values
-if [ -n "$IPHONEOS_DEPLOYMENT_TARGET" ]; then
-find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.ios\.deployment_target = .*/s.ios.deployment_target = \'$IPHONEOS_DEPLOYMENT_TARGET\'/g" {} \;
+PODSPEC="$SRCROOT/RxSwiftAddons.podspec"
+IOS_VERSION="$IPHONEOS_DEPLOYMENT_TARGET"
+MACOS_VERSION="$MACOSX_DEPLOYMENT_TARGET"
+WHATCHOS_VERSION="$WATCHOS_DEPLOYMENT_TARGET"
+TVOS_VERSION="$TVOS_DEPLOYMENT_TARGET"
+VERSION="$VERSION_STRING"
+
+if [ -n "$IOS_VERSION" ]; then
+MATCHSTRING="s.ios.deployment_target = "
+REPLACESTRING="s.ios.deployment_target = \'$IOS_VERSION\'"
+sed -i '' -e "s/$MATCHSTRING.*/$REPLACESTRING/g" "$PODSPEC"
 fi
 
-if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
-find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.osx\.deployment_target = .*/s.osx.deployment_target = \'$MACOSX_DEPLOYMENT_TARGET\'/g" {} \;
+if [ -n "$MACOS_VERSION" ]; then
+MATCHSTRING="s.osx.deployment_target = "
+REPLACESTRING="s.osx.deployment_target = \'$MACOS_VERSION\'"
+sed -i '' -e "s/$MATCHSTRING.*/$REPLACESTRING/g" "$PODSPEC"
 fi
 
-if [ -n "$WATCHOS_DEPLOYMENT_TARGET" ]; then
-find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.watchos\.deployment_target = .*/s.watchos.deployment_target = \'$WATCHOS_DEPLOYMENT_TARGET\'/g" {} \;
+if [ -n "$WHATCHOS_VERSION" ]; then
+MATCHSTRING="s.watchos.deployment_target = "
+REPLACESTRING="s.watchos.deployment_target = \'$WHATCHOS_VERSION\'"
+sed -i '' -e "s/$MATCHSTRING.*/$REPLACESTRING/g" "$PODSPEC"
 fi
 
-if [ -n "$TVOS_DEPLOYMENT_TARGET" ]; then
-find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.tvos\.deployment_target = .*/s.tvos.deployment_target = \'$TVOS_DEPLOYMENT_TARGET\'/g" {} \;
+if [ -n "$TVOS_VERSION" ]; then
+MATCHSTRING="s.tvos.deployment_target = "
+REPLACESTRING="s.tvos.deployment_target = \'$TVOS_VERSION\'"
+sed -i '' -e "s/$MATCHSTRING.*/$REPLACESTRING/g" "$PODSPEC"
 fi
 
-if [ -n "$VERSION_STRING" ]; then
-find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s.version          = .*/s.version          = \"$VERSION_STRING\"/g" {} \;
+if [ -n "$VERSION" ]; then
+MATCHSTRING="s.version          = "
+REPLACESTRING="s.version          = \"$VERSION\""
+sed -i '' -e "s/$MATCHSTRING.*/$REPLACESTRING/g" "$PODSPEC"
 fi
