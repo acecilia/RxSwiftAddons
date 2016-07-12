9e538d61f1aeecc0c14cce50d76e15801211cc08
acecilia <a.cecilia.luque@gmail.com>
2016-07-12 04:03:26 +0200

    Added postBuild script, so automatically changes deployment_target for each platform based on the deployment_target value in xcode

--- postBuild.sh
+++ postBuild.sh
@@ -1,2 +1,19 @@
+#Throws warnings for TODO: and FIXME: comments
 TAGS="TODO:|FIXME:"
 find "${SRCROOT}/Sources" \( -name "*.h" -or -name "*.m" -or -name "*.swift" \) -print0 | xargs -0 egrep -s --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
+
+if [ -n "$IPHONEOS_DEPLOYMENT_TARGET" ]; then
+find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.ios\.deployment_target = .*/s.ios.deployment_target = \'$IPHONEOS_DEPLOYMENT_TARGET\'/g" {} \;
+fi
+
+if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
+find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.osx\.deployment_target = .*/s.osx.deployment_target = \'$MACOSX_DEPLOYMENT_TARGET\'/g" {} \;
+fi
+
+if [ -n "$WATCHOS_DEPLOYMENT_TARGET" ]; then
+find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.watchos\.deployment_target = .*/s.watchos.deployment_target = \'$WATCHOS_DEPLOYMENT_TARGET\'/g" {} \;
+fi
+
+if [ -n "$TVOS_DEPLOYMENT_TARGET" ]; then
+find "${SRCROOT}/RxSwiftAddons.podspec" -exec sed -i '' -e "s/s\.tvos\.deployment_target = .*/s.tvos.deployment_target = \'$TVOS_DEPLOYMENT_TARGET\'/g" {} \;
+fi
